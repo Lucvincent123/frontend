e5de7f6afdd04e88c182e2bcd93d1b6888fc9d1b
Neiluj35 <julien35.brelivet@gmail.com>
2025-04-01 11:45:06 +0200

    modif de l erreur de merge

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -60,7 +60,7 @@ const Timeline = () => {
                         className={`event ${event.fixed ? 'fixed-event' : ''}`}
                         style={{ left: `${event.position}px` }}
                     >
-                      {event.text}
+                        {event.text}
                     </div>
                 ))}
                 {droppedPosition !== null && (
@@ -68,15 +68,8 @@ const Timeline = () => {
                         className={`event proposed-event ${isCorrect !== null ? (isCorrect ? 'correct' : 'incorrect') : ''}`}
                         style={{ left: `${droppedPosition}px` }}
                     >
-                      {proposedEvent.text}
+                        {proposedEvent.text}
                     </div>
-                  </div>
-                    // <div
-                    //     className={`event proposed-event ${isCorrect !== null ? (isCorrect ? 'correct' : 'incorrect') : ''}`}
-                    //     style={{ left: `${proposedEvent.position}px` }}
-                    // >
-                    //     {proposedEvent.text}
-                    // </div>
                 )}
             </div>
             {proposedEvent && droppedPosition === null && (
@@ -85,15 +78,6 @@ const Timeline = () => {
                         {proposedEvent.text}
                     </div>
                 </div>
-                // <div
-                //     className="proposed-event-container"
-                //     draggable
-                //     onDragStart={(e) => handleDragStart(e, proposedEvent.id)}
-                // >
-                //     <div className="event proposed-event">
-                //         {proposedEvent.text}
-                //     </div>
-                // </div>
             )}
         </div>
     );
